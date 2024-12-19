ffd4407cf0d66d52188cd90b757afee6f75ddedc
technolab132 <krishnawankhede2020@gmail.com>
2024-12-19 10:41:44 +0530

    adfdafs

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-from app import st
-import streamlit.web.bootstrap as bootstrap
-
-def handler(event, context):
-    return bootstrap.run(st, '', '', '')
-
-if __name__ == '__main__':
-    bootstrap.run(st, '', '', '')
